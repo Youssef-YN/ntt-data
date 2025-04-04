fa75df505d7704e253d9a45938902e29cfa0e787
Youssef <nazihyoussef05@gmail.com>
2025-04-04 09:47:35 +0000

    Set up home page and top bar further

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 import { ButtonModule } from 'primeng/button';
+import { LayoutService } from "../../layout/service/app.layout.service";
+import { Router } from "@angular/router";
+import { DividerModule } from "primeng/divider";
+import { StyleClassModule } from "primeng/styleclass";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, DividerModule, CommonModule, StyleClassModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+  constructor(public layoutService: LayoutService, public router: Router) {
+  }
 }
